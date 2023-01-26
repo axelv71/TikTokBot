052ff15c87bd906791257c7cf1ca3cd4b9d25249
Axel Vion <axel.vionyt@gmail.com>
2023-01-26 19:18:30 +0100

    :barber: Progress bar :barber:

--- Controller/Downloader/Downloader.py
+++ Controller/Downloader/Downloader.py
@@ -1,3 +1,5 @@
+import sys
+
 from pytube import YouTube
 from cleantext import clean
 
@@ -10,13 +12,13 @@ class Downloader:
 
     def download(self):
         print(f'Downloading {self.url}...')
-        youtube = YouTube(self.url)
+        youtube = YouTube(self.url, on_progress_callback=self.progress)
         youtube = youtube.streams.get_highest_resolution()
         self.name = self.format_name(youtube.default_filename)
         print(self.name)
         try:
             youtube.download(self.path, self.name)
-            print(f'Downloaded {self.name} successfully!')
+            print(f'\nDownloaded {self.name} successfully!')
         except:
             print("Error downloading video")
 
@@ -24,5 +26,11 @@ class Downloader:
         formatted_name: str = clean(name, to_ascii=True, lower=True, no_line_breaks=True, no_urls=True, no_emoji=True)
         formatted_name = formatted_name.replace(' ', '_')
         return formatted_name
+
+    def progress(self, stream, chunk, bytes_remaining):
+        percent = int((stream.filesize - bytes_remaining) / stream.filesize * 100)
+        sys.stdout.write(f'\rProgress: {percent}%')
+        sys.stdout.flush()
+
 
 
